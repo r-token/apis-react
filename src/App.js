a966ee1a5b6e4bb8b2bd172e8a502299e93301ab
Ryan Token <ryantoken13@gmail.com>
2020-08-21 16:32:34 -0500

    Added basic API call and response with openWeatherMap's API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null)
+  const [weatherDataIsLoaded, setWeatherDataIsLoaded] = useState(false)
+  const [weatherData, setWeatherData] = useState([])
+
+  const apiKey = 'YourApiKey' // replace this with your API key from https://home.openweathermap.org/api_keys
+  const cityName = 'Tulsa'
+
+  // useEffect with an empty [] at the end runs once on page load.
+  // put all the 'fetch' code in a button if you'd rather load data on button click
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`)
+      .then(res => res.json())
+      .then( 
+        (result) => {
+          setWeatherDataIsLoaded(true)
+          setWeatherData(result)
+        },
+        (error) => {
+          setWeatherDataIsLoaded(true)
+          setError(error)
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+
+  } else if (!weatherDataIsLoaded) {
+      return <div>Loading...</div>;
+
+  } else if ('main' in weatherData) {
+      return <div>Current temperature in {cityName}: {weatherData.main.temp}° Fahrenheit</div>
+
+  } else {
+    return <div>Loading...</div>;
+  }
+
 }
 
 export default App;
